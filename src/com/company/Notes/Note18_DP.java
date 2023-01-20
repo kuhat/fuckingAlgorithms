a31d66d9fb09f1ef2da05721628d5b531e8fc273
Danny Hao <1043744621@qq.com>
2023-01-20 09:27:19 -0500

    1/20

--- src/com/company/Notes/Note18_DP.java
+++ src/com/company/Notes/Note18_DP.java
@@ -2776,6 +2776,46 @@ public class Note18_DP {
         }
     }
 
+    // 1048: Longest String China
+
+    /**
+     * You are given an array of words where each word consists of lowercase English letters.
+     *
+     * wordA is a predecessor of wordB if and only if we can insert exactly one letter anywhere in wordA without
+     * changing the order of the other characters to make it equal to wordB.
+     *
+     * For example, "abc" is a predecessor of "abac", while "cba" is not a predecessor of "bcad".
+     * A word chain is a sequence of words [word1, word2, ..., wordk] with k >= 1, where word1 is a predecessor
+     * of word2, word2 is a predecessor of word3, and so on. A single word is trivially a word chain with k == 1.
+     *
+     * Return the length of the longest possible word chain with words chosen from the given list of words.
+     */
+    class Solution1048 {
+        public int longestStrChain(String[] words) {
+            Map<String, Integer> dp = new HashMap<>();
+
+            // Sorting the list in terms of the word length.
+            Arrays.sort(words, (a, b) -> a.length() - b.length());
+
+            int res = 1;
+
+            for (String word : words) {
+                int presentLength = 1;
+                // Find all possible predecessors for the current word by removing one letter at a time.
+                for (int i = 0; i < word.length(); i++) {
+                    StringBuilder temp = new StringBuilder(word);
+                    temp.deleteCharAt(i);
+                    String predecessor = temp.toString();
+                    int previousLength = dp.getOrDefault(predecessor, 0);
+                    presentLength = Math.max(presentLength, previousLength + 1);
+                }
+                dp.put(word, presentLength);
+                res = Math.max(res, presentLength);
+            }
+            return res;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(Arrays.toString(new int[5]));
     }
