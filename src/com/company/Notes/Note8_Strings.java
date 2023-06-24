cbc98fb185aa89653f8f0e97f2f439ce79d42618
Danny Hao <1043744621@qq.com>
2023-06-24 16:42:07 -0400

    6.24

--- src/com/company/Notes/Note8_Strings.java
+++ src/com/company/Notes/Note8_Strings.java
@@ -3579,6 +3579,45 @@ public class Note8_Strings {
 // Codec codec = new Codec();
 // codec.decode(codec.encode(strs));
 
+    // 2486: append Characters to string to make subsequence
+
+    /**
+     * You are given two strings s and t consisting of only lowercase English letters.
+     *
+     * Return the minimum number of characters that need to be appended to the end of s so that t becomes a subsequence of s.
+     *
+     * A subsequence is a string that can be derived from another string by deleting some or no characters without changing
+     * the order of the remaining characters.
+     *
+     * Example 1:
+     *
+     * Input: s = "coaching", t = "coding"
+     * Output: 4
+     * Explanation: Append the characters "ding" to the end of s so that s = "coachingding".
+     * Now, t is a subsequence of s ("coachingding").
+     * It can be shown that appending any 3 characters to the end of s will never make t a subsequence.
+     * Example 2:
+     *
+     * Input: s = "abcde", t = "a"
+     * Output: 0
+     * Explanation: t is already a subsequence of s ("abcde").
+     * @param args
+     */
+    class Solution2486 {
+        public int appendCharacters(String s, String t) {
+            int sp = 0, tp = 0, res = 0;
+            while (sp < s.length() && tp < t.length()) {
+                if (s.charAt(sp) == t.charAt(tp)) {
+                    sp++;
+                    tp++;
+                } else {
+                    sp++;
+                }
+            }
+            return t.length() - tp;
+        }
+    }
+
     public static void main(String[] args) {
         String[] words = new String[]{"lc", "cl", "gg"};
         ArrayList sdf = new ArrayList();
