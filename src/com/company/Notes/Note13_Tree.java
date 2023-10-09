9c46a82b862da26f3744640b95529fa23682f55d
Danny Hao <1043744621@qq.com>
2023-10-08 21:25:05 -0400

    10/8

--- src/com/company/Notes/Note13_Tree.java
+++ src/com/company/Notes/Note13_Tree.java
@@ -2390,28 +2390,174 @@ c. Case #3:
         }
     }
 
+    /**
+     * 1302: deepest leaves sum
+     * Given the root of a binary tree, return the sum of values of its deepest leaves.
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+     * Output: 15
+     * Example 2:
+     *
+     * Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+     * Output: 19
+     */
+    class Solution1302 {
+        public int deepestLeavesSum(TreeNode root) {
+            // level order traversal, record level sum, return the sum of end level
+            Queue<TreeNode> qu = new LinkedList<>();
+            qu.offer(root);
+            int res = 0;
+            while (!qu.isEmpty()) {
+                int curSum = 0;
+                int n = qu.size();
+                for (int i = 0; i < n; i ++) {
+                    TreeNode cur = qu.poll();
+                    curSum += cur.val;
+                    if (cur.left != null) qu.offer(cur.left);
+                    if (cur.right != null) qu.offer(cur.right);
+                }
+                res = curSum;
+            }
+            return res;
+        }
+    }
 
+    // 1973: Count nodes equal to Sum of Descedants
 
+    /**
+     * Given the root of a binary tree, return the number of nodes where the value of the node is equal to the sum of
+     * the values of its descendants.
+     *
+     * A descendant of a node x is any node that is on the path from node x to some leaf node. The sum is considered
+     * to be 0 if the node has no descendants.
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [10,3,4,2,1]
+     * Output: 2
+     * Explanation:
+     * For the node with value 10: The sum of its descendants is 3+4+2+1 = 10.
+     * For the node with value 3: The sum of its descendants is 2+1 = 3.
+     * @param n
+     * @param k
+     */
+    class Solution1973 {
+        int count = 0;
+        public int equalToDescendants(TreeNode root) {
+            helper(root);
+            return count;
+        }
 
+        public int helper(TreeNode root) {
+            if (root == null) return 0;
+            int left = helper(root.left);
+            int right =helper(root.right);
+            if (root.val == left + right) {
+                count ++;
+            }
+            return left + right + root.val;
+        }
 
+    }
 
+    /**
+     * 2458. Height of Binary Tree After Subtree Removal Queries
+     *
+     * You are given the root of a binary tree with n nodes. Each node is assigned a unique value from 1 to n. You are also given an array queries of size m.
+     *
+     * You have to perform m independent queries on the tree where in the ith query you do the following:
+     *
+     * Remove the subtree rooted at the node with the value queries[i] from the tree. It is guaranteed that queries[i] will not be equal to the value of the root.
+     * Return an array answer of size m where answer[i] is the height of the tree after performing the ith query.
+     *
+     * Note:
+     *
+     * The queries are independent, so the tree returns to its initial state after each query.
+     * The height of a tree is the number of edges in the longest simple path from the root to some node in the tree.
+     *
+     *
+     * Example 1:
+     *
+     *
+     * Input: root = [1,3,4,2,null,6,5,null,null,null,null,null,7], queries = [4]
+     * Output: [2]
+     * Explanation: The diagram above shows the tree after removing the subtree rooted at node with value 4.
+     * The height of the tree is 2 (The path 1 -> 3 -> 2).
+     * Example 2:
+     *
+     *
+     * Input: root = [5,8,9,2,1,3,7,4,6], queries = [3,2,4,8]
+     * Output: [3,2,3,2]
+     * Explanation: We have the following queries:
+     * - Removing the subtree rooted at node with value 3. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 4).
+     * - Removing the subtree rooted at node with value 2. The height of the tree becomes 2 (The path 5 -> 8 -> 1).
+     * - Removing the subtree rooted at node with value 4. The height of the tree becomes 3 (The path 5 -> 8 -> 2 -> 6).
+     * - Removing the subtree rooted at node with value 8. The height of the tree becomes 2 (The path 5 -> 9 -> 3).
+     * @param n
+     * @param k
+     */
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution2458 {
+        private Map<Integer, Integer> leftMap = new HashMap<>();
+        private Map<Integer, Integer> rightMap = new HashMap<>();
+        private Map<Integer, Integer> removed = new HashMap<>();
 
+        public int[] treeQueries(TreeNode root, int[] queries) {
+            populateHeights(root, 0);
+            calculateRemovedHeights(root, 0);
 
+            int[] output = new int[queries.length];
+            for (int i = 0; i < queries.length; i++) {
+                output[i] = removed.get(queries[i]);
+            }
+            return output;
+        }
 
+        // height is the max tree height with this node removed
+        private void calculateRemovedHeights(TreeNode node, int height) {
+            if (node == null) {
+                return;
+            }
+            removed.put(node.val, height);
 
+            // for each child, the height when removed is the max of the the height following
+            // the opposite child, or the passed-in height with this node removed
+            calculateRemovedHeights(node.left, Math.max(height, rightMap.get(node.val)));
+            calculateRemovedHeights(node.right, Math.max(height, leftMap.get(node.val)));
+        }
 
+        // populate the maps with the total height of the left and right subtree of
+        // each node, and return the larger of the two values
+        private int populateHeights(TreeNode node, int height) {
+            if (node == null) {
+                return height - 1;
+            }
 
+            leftMap.put(node.val, populateHeights(node.left, height + 1));
+            rightMap.put(node.val, populateHeights(node.right, height + 1));
 
-
-
-
-
-
-
-
-
-
-
+            return Math.max(leftMap.get(node.val), rightMap.get(node.val));
+        }
+    }
 
 
     public static void sol(int n, int k) {
